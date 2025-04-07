53f25fec856b82986d99794879c9113825ae09e1
zwivhuyamashau <zwivhuya@gmail.com>
2025-04-07 21:12:40 +0200

    Update config.js
    update the API gateway endpoints

--- src/config.js
+++ src/config.js
@@ -1,17 +1,2 @@
-// Replace 'YOUR_API_KEY' with your actual Google Maps API key
-//export const GOOGLE_MAPS_API_KEY = "XXXXXXXXX"
+
 export const APIGATEWAY_BASE = 'https://XXXXXXXX.execute-api.us-east-1.amazonaws.com/test/'
-
-// Other configuration options
-//export const DEFAULT_SEARCH_RADIUS = 5000; // 5km
-//export const MAX_SEARCH_RADIUS = 50000; // 50km
-//export const DEFAULT_RESULTS_LIMIT = 20;
-
-// API endpoints
-/*
-export const API_CONFIG = {
-  //PLACES_API_BASE_URL: 'https://maps.googleapis.com/maps/api/place'
-  //PLACES_API_BASE_URL: `${APIGATEWAY_BASE}places`,
-  //GEOCODING_API_BASE_URL: 'https://maps.googleapis.com/maps/api/geocode'
-};
-*/
